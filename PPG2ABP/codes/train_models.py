5d97aea08884cff2ac3ece30b579c39771538bac
Xteom <mateodelaroche@gmail.com>
2023-08-31 20:37:50 -0600

    train_models.py cleaned

--- PPG2ABP/codes/train_models.py
+++ PPG2ABP/codes/train_models.py
@@ -49,19 +49,19 @@ def train_approximate_network():
     for foldname in range(10):
 
         print('----------------')
-        print('Training Fold {}'.format(foldname+1))
+        print(f'Training Fold {foldname}')
         print('----------------')
                                                                                             # loading training data
-        dt = pickle.load(open(os.path.join('data','train{}.p'.format(foldname)),'rb'))
+        dt = pickle.load(open(os.path.join('data',f'train{foldname}.p'),'rb'))
         X_train = dt['X_train']
         Y_train = dt['Y_train']
                                                                                             # loading validation data
-        dt = pickle.load(open(os.path.join('data','val{}.p'.format(foldname)),'rb'))
+        dt = pickle.load(open(os.path.join('data',f'val{foldname}.p'),'rb'))
         X_val = dt['X_val']
         Y_val = dt['Y_val']
 
                                                                                             # loading metadata
-        dt = pickle.load(open(os.path.join('data','meta{}.p'.format(foldname)),'rb'))
+        dt = pickle.load(open(os.path.join('data',f'meta{foldname}.p'),'rb'))
         max_ppg = dt['max_ppg']
         min_ppg = dt['min_ppg']
         max_abp = dt['max_abp']
@@ -80,11 +80,11 @@ def train_approximate_network():
         mdl1.compile(loss='mean_absolute_error',optimizer='adam',metrics=['mean_squared_error'], loss_weights=[1., 0.9, 0.8, 0.7, 0.6])                                                         
 
 
-        checkpoint1_ = ModelCheckpoint(os.path.join('models','{}_model1_fold{}.h5'.format(mdlName1,foldname)), verbose=1, monitor='val_out_loss',save_best_only=True, mode='auto')  
+        checkpoint1_ = ModelCheckpoint(os.path.join('models',f'{mdlName1}_model1_fold{foldname}.h5'), verbose=1, monitor='val_out_loss',save_best_only=True, mode='auto')  
                                                                         # train approximation network for 100 epochs
         history1 = mdl1.fit(X_train,{'out': Y_train['out'], 'level1': Y_train['level1'], 'level2':Y_train['level2'], 'level3':Y_train['level3'] , 'level4':Y_train['level4']},epochs=100,batch_size=256,validation_data=(X_val,{'out': Y_val['out'], 'level1': Y_val['level1'], 'level2':Y_val['level2'], 'level3':Y_val['level3'] , 'level4':Y_val['level4']}),callbacks=[checkpoint1_],verbose=1)
 
-        pickle.dump(history1, open('History/{}_model1_fold{}.p'.format(mdlName1,foldname),'wb'))    # save training history
+        pickle.dump(history1, open(f'History/{mdlName1}_model1_fold{foldname}.p','wb'))    # save training history
 
 
         mdl1 = None                                             # garbage collection
@@ -121,16 +121,16 @@ def train_refinement_network():
         print('Training Fold {}'.format(foldname+1))
         print('----------------')
                                                                                             # loading training data
-        dt = pickle.load(open(os.path.join('data','train{}.p'.format(foldname)),'rb'))
+        dt = pickle.load(open(os.path.join('data',f'train{foldname}.p'),'rb'))
         X_train = dt['X_train']
         Y_train = dt['Y_train']
                                                                                             # loading validation data
-        dt = pickle.load(open(os.path.join('data','val{}.p'.format(foldname)),'rb'))
+        dt = pickle.load(open(os.path.join('data',f'val{foldname}.p'),'rb'))
         X_val = dt['X_val']
         Y_val = dt['Y_val']
 
                                                                                             # loading metadata
-        dt = pickle.load(open(os.path.join('data','meta{}.p'.format(foldname)),'rb'))
+        dt = pickle.load(open(os.path.join('data',f'meta{foldname}.p'),'rb'))
         max_ppg = dt['max_ppg']
         min_ppg = dt['min_ppg']
         max_abp = dt['max_abp']
@@ -143,7 +143,7 @@ def train_refinement_network():
     
     
         mdl1 = model_dict[mdlName1](length)                 # load approximation network
-        mdl1.load_weights(os.path.join('models','{}_model1_fold{}.h5'.format(mdlName1,foldname)))   # load weights
+        mdl1.load_weights(os.path.join('models',f'{mdlName1}_model1_fold{foldname}.h5'))   # load weights
 
         X_train = prepareDataDS(mdl1, X_train)          # prepare training data for 2nd stage, considering deep supervision
         X_val = prepareDataDS(mdl1, X_val)              # prepare validation data for 2nd stage, considering deep supervision
@@ -156,12 +156,12 @@ def train_refinement_network():
                                                                     # loss = mse
         mdl2.compile(loss='mean_squared_error',optimizer='adam',metrics=['mean_absolute_error'])
 
-        checkpoint2_ = ModelCheckpoint(os.path.join('models','{}_model2_fold{}.h5'.format(mdlName2,foldname)), verbose=1, monitor='val_loss',save_best_only=True, mode='auto')  
+        checkpoint2_ = ModelCheckpoint(os.path.join('models',f'{mdlName2}_model2_fold{foldname}.h5'), verbose=1, monitor='val_loss',save_best_only=True, mode='auto')  
 
                                                                 # train refinement network for 100 epochs
         history2 = mdl2.fit(X_train,Y_train['out'],epochs=100,batch_size=192,validation_data=(X_val,Y_val['out']),callbacks=[checkpoint2_])
 
-        pickle.dump(history2, open('History/{}_model2_fold{}.p'.format(mdlName2,foldname),'wb'))    # save training history
+        pickle.dump(history2, open(f'History/{mdlName2}_model2_fold{foldname}.p','wb'))    # save training history
 
         time.sleep(300)                                         # pause execution for a while to free the gpu
 
